8935311a1574bb79b1aec866b1bc7b388570a41a
DunnTapCodee <dunnbxvp@gmail.com>
2025-10-01 00:16:06 +0700

    connect to mySQL Database

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,8 +1,21 @@
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+import db from '../models/index';
+
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log('-------------------');
+        console.log(data);
+        console.log('-------------------');
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
 }
 
-let getAboutPage = (req, res) =>{
+let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 // Object : {
@@ -12,5 +25,5 @@ let getAboutPage = (req, res) =>{
 
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage : getAboutPage
+    getAboutPage: getAboutPage
 }
