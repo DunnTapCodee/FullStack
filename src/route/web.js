3a36df3da5bd91ca528317ba7fb133f268a95f94
DunnTapCodee <dunnbxvp@gmail.com>
2025-09-30 23:18:24 +0700

    Create Table, Add Database With Sequelize CLI

--- src/route/web.js
+++ src/route/web.js
@@ -1,11 +1,18 @@
 import express from "express";
-
+import homeControllers from "../controllers/homeControllers";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get('/', (req, res) => {
-        return res.send("Hello World with DunnTapCode")
+    router.get('/', homeControllers.getHomePage);
+    router.get('/about', homeControllers.getAboutPage);
+
+    router.get('/dunntapcode', (req, res) => {
+        return res.send("Hello World with DunnTapCode hehehe")
     });
+
+    // rest api 
+
+
     return app.use("/", router);
 }
 
